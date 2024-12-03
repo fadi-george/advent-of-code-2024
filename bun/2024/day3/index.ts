ea23a54cd7d09f850e7154f684cca69e975b6995
Fadi George <fadii925@gmail.com>
2024-12-02 22:00:43 -0800

    more clean up

--- bun/2024/day3/index.ts
+++ bun/2024/day3/index.ts
@@ -36,32 +36,27 @@ This time, the sum of the results is 48 (2*4 + 8*5).
 
 Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
 */
-const part1Values: [number, number][] = [];
-const part2Values: [number, number][] = [];
+const p1Vals: [number, number][] = [];
+const p2Vals: [number, number][] = [];
 
 let ignored = false;
 readFile(import.meta.dir).forEach((line) => {
   const matches = line.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g) || [];
   for (const match of matches) {
     const [command, x, y] = match.values();
-    if (command === "do()") {
-      ignored = false;
-    } else if (command === "don't()") {
-      ignored = true;
-    } else {
-      part1Values.push([+x, +y]);
-      if (!ignored) part2Values.push([+x, +y]);
+
+    if (command === "do()") ignored = false;
+    else if (command === "don't()") ignored = true;
+    else {
+      p1Vals.push([+x, +y]);
+      if (!ignored) p2Vals.push([+x, +y]);
     }
   }
 });
 
-const part1 = () => {
-  return part1Values.reduce((acc, [x, y]) => acc + x * y, 0);
-};
+const part1 = () => p1Vals.reduce((acc, [x, y]) => acc + x * y, 0);
 
-const part2 = () => {
-  return part2Values.reduce((acc, [x, y]) => acc + x * y, 0);
-};
+const part2 = () => p2Vals.reduce((acc, [x, y]) => acc + x * y, 0);
 
 console.log("Part 1: ", part1());
 console.log("Part 2: ", part2());
