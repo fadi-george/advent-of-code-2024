fc75704940c267bf30450d8d95d33d6f647de7c5
Fadi George <fadii925@gmail.com>
2024-12-03 22:39:26 -0800

    more day4 clean up

--- aoc/2024/day4/index.ts
+++ aoc/2024/day4/index.ts
@@ -1,48 +1,37 @@
 export default (input: string) => {
-  const grid: string[][] = [];
-  input.split("\n").forEach((line) => {
-    grid.push(line.split(""));
-  });
+  const grid = input.split("\n").map((line) => line.split(""));
 
   const part1 = () => {
     let count = 0;
+    const directions = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+      [1, 1],
+      [1, -1],
+      [-1, 1],
+      [-1, -1],
+    ];
 
     const checkDirection = (i: number, j: number, dx: number, dy: number) => {
-      let str = "X";
+      let str = "";
       for (let k = 1; k < 4; k++) {
         const row = i + k * dy;
         const col = j + k * dx;
         if (!grid[row]?.[col]) return false;
         str += grid[row][col];
       }
-      return str === "XMAS";
+      return str === "MAS";
     };
-
-    const checkRight = (i: number, j: number) => checkDirection(i, j, 1, 0);
-    const checkLeft = (i: number, j: number) => checkDirection(i, j, -1, 0);
-    const checkDown = (i: number, j: number) => checkDirection(i, j, 0, 1);
-    const checkUp = (i: number, j: number) => checkDirection(i, j, 0, -1);
-    const checkDownRight = (i: number, j: number) => checkDirection(i, j, 1, 1);
-    const checkUpRight = (i: number, j: number) => checkDirection(i, j, 1, -1);
-    const checkDownLeft = (i: number, j: number) => checkDirection(i, j, -1, 1);
-    const checkUpLeft = (i: number, j: number) => checkDirection(i, j, -1, -1);
 
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[i].length; j++) {
         if (grid[i][j] !== "X") continue;
 
-        [
-          checkRight,
-          checkLeft,
-          checkDown,
-          checkUp,
-          checkDownRight,
-          checkUpRight,
-          checkDownLeft,
-          checkUpLeft,
-        ].forEach((check) => {
-          if (check(i, j)) count++;
-        });
+        for (const [dx, dy] of directions) {
+          if (checkDirection(i, j, dx, dy)) count++;
+        }
       }
     }
     return count;
