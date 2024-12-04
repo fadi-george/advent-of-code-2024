aa2707d18234bbb98c54e2925c8905a6246ff2d8
Fadi George <fadii925@gmail.com>
2024-12-03 21:25:38 -0800

    p2 wip

--- aoc/2024/day4/index.ts
+++ aoc/2024/day4/index.ts
@@ -6,114 +6,144 @@ export default (input: string) => {
 
   // console.log(grid);
 
-  const checkRight = (i: number, j: number) => {
-    let str = "X";
-    for (let k = j + 1; k < j + 4; k++) {
-      if (!grid[i][k]) return false;
-      str += grid[i][k];
+  const part1 = () => {
+    const checkRight = (i: number, j: number) => {
+      let str = "X";
+      for (let k = j + 1; k < j + 4; k++) {
+        if (!grid[i][k]) return false;
+        str += grid[i][k];
+      }
+      return str === "XMAS";
+    };
+
+    const checkLeft = (i: number, j: number) => {
+      let str = "X";
+      for (let k = j - 1; k >= j - 3; k--) {
+        if (!grid[i][k]) return false;
+        str += grid[i][k];
+      }
+      return str === "XMAS";
+    };
+
+    const checkDown = (i: number, j: number) => {
+      let str = "X";
+      for (let k = i + 1; k < i + 4; k++) {
+        if (!grid[k]) return false;
+        if (!grid[k][j]) return false;
+        str += grid[k][j];
+      }
+      return str === "XMAS";
+    };
+
+    const checkUp = (i: number, j: number) => {
+      let str = "X";
+      for (let k = i - 1; k >= i - 3; k--) {
+        if (!grid[k]) return false;
+        if (!grid[k][j]) return false;
+        str += grid[k][j];
+      }
+      return str === "XMAS";
+    };
+
+    const checkDownRight = (i: number, j: number) => {
+      let str = "X";
+      for (let k = 1; k < 4; k++) {
+        if (!grid[i + k]) return false;
+        if (!grid[i + k][j + k]) return false;
+        str += grid[i + k][j + k];
+      }
+      return str === "XMAS";
+    };
+
+    const checkUpRight = (i: number, j: number) => {
+      let str = "X";
+      for (let k = 1; k < 4; k++) {
+        if (!grid[i - k]) return false;
+        if (!grid[i - k][j + k]) return false;
+        str += grid[i - k][j + k];
+      }
+      return str === "XMAS";
+    };
+
+    const checkDownLeft = (i: number, j: number) => {
+      let str = "X";
+      for (let k = 1; k < 4; k++) {
+        if (!grid[i + k]) return false;
+        if (!grid[i + k][j - k]) return false;
+        str += grid[i + k][j - k];
+      }
+      return str === "XMAS";
+    };
+
+    const checkUpLeft = (i: number, j: number) => {
+      let str = "X";
+      for (let k = 1; k < 4; k++) {
+        if (!grid[i - k]) return false;
+        if (!grid[i - k][j - k]) return false;
+        str += grid[i - k][j - k];
+      }
+      return str === "XMAS";
+    };
+
+    const indicies: [number, number][] = [];
+
+    let count = 0;
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] !== "X") continue;
+
+        [
+          checkRight,
+          checkLeft,
+          checkDown,
+          checkUp,
+          checkDownRight,
+          checkUpRight,
+          checkDownLeft,
+          checkUpLeft,
+        ].forEach((check) => {
+          if (check(i, j)) {
+            indicies.push([i, j]);
+            count++;
+          }
+        });
+      }
     }
-    return str === "XMAS";
+    console.log(count);
   };
 
-  const checkLeft = (i: number, j: number) => {
-    let str = "X";
-    for (let k = j - 1; k >= j - 3; k--) {
-      if (!grid[i][k]) return false;
-      str += grid[i][k];
-    }
-    return str === "XMAS";
-  };
+  const part2 = () => {
+    let count = 0;
+    const checkPattern1 = (i: number, j: number) => {
+      let mCount = 0;
+      let sCount = 0;
 
-  const checkDown = (i: number, j: number) => {
-    let str = "X";
-    for (let k = i + 1; k < i + 4; k++) {
-      if (!grid[k]) return false;
-      if (!grid[k][j]) return false;
-      str += grid[k][j];
-    }
-    return str === "XMAS";
-  };
-
-  const checkUp = (i: number, j: number) => {
-    let str = "X";
-    for (let k = i - 1; k >= i - 3; k--) {
-      if (!grid[k]) return false;
-      if (!grid[k][j]) return false;
-      str += grid[k][j];
-    }
-    return str === "XMAS";
-  };
-
-  const checkDownRight = (i: number, j: number) => {
-    let str = "X";
-    for (let k = 1; k < 4; k++) {
-      if (!grid[i + k]) return false;
-      if (!grid[i + k][j + k]) return false;
-      str += grid[i + k][j + k];
-    }
-    return str === "XMAS";
-  };
-
-  const checkUpRight = (i: number, j: number) => {
-    let str = "X";
-    for (let k = 1; k < 4; k++) {
-      if (!grid[i - k]) return false;
-      if (!grid[i - k][j + k]) return false;
-      str += grid[i - k][j + k];
-    }
-    return str === "XMAS";
-  };
-
-  const checkDownLeft = (i: number, j: number) => {
-    let str = "X";
-    for (let k = 1; k < 4; k++) {
-      if (!grid[i + k]) return false;
-      if (!grid[i + k][j - k]) return false;
-      str += grid[i + k][j - k];
-    }
-    return str === "XMAS";
-  };
-
-  const checkUpLeft = (i: number, j: number) => {
-    let str = "X";
-    for (let k = 1; k < 4; k++) {
-      if (!grid[i - k]) return false;
-      if (!grid[i - k][j - k]) return false;
-      str += grid[i - k][j - k];
-    }
-    return str === "XMAS";
-  };
-
-  const indicies: [number, number][] = [];
-
-  let count = 0;
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] !== "X") continue;
-
+      // corners
       [
-        checkRight,
-        checkLeft,
-        checkDown,
-        checkUp,
-        checkDownRight,
-        checkUpRight,
-        checkDownLeft,
-        checkUpLeft,
-      ].forEach((check) => {
-        if (check(i, j)) {
-          indicies.push([i, j]);
+        [i - 1, j - 1],
+        [i - 1, j + 1],
+        [i + 1, j - 1],
+        [i + 1, j + 1],
+      ].forEach(([x, y]) => {
+        if (!grid[x]) return;
+        if (!grid[x][y]) return;
+        if (grid[x][y] === "M") mCount++;
+        if (grid[x][y] === "S") sCount++;
+      });
+
+      return mCount === 2 && sCount === 2;
+    };
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j] !== "A") continue;
+        if (checkPattern1(i, j)) {
           count++;
         }
-      });
+      }
     }
-  }
-
-  console.log(count);
-
-  const part1 = () => {};
-
-  const part2 = () => {};
+    console.log(count);
+  };
 
   return {
     part1: part1(),
