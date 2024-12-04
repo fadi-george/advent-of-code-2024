3000dedb7de6fedde1cd6533b1715ff4f47309a3
Fadi George <fadii925@gmail.com>
2024-12-04 00:32:37 -0800

    dont need split or str additional logic

--- aoc/2024/day4/index.ts
+++ aoc/2024/day4/index.ts
@@ -1,68 +1,51 @@
 export default (input: string) => {
-  const grid = input.split("\n").map((line) => line.split(""));
+  const grid = input.split("\n").map((line) => line);
 
-  const part1 = () => {
-    let count = 0;
-    const directions = [
-      [1, 0],
-      [-1, 0],
-      [0, 1],
-      [0, -1],
-      [1, 1],
-      [1, -1],
-      [-1, 1],
-      [-1, -1],
-    ];
+  let p1Count = 0;
+  let p2Count = 0;
 
-    const checkDirection = (i: number, j: number, dx: number, dy: number) => {
-      let str = "";
-      for (let k = 1; k < 4; k++) {
-        const row = i + k * dy;
-        const col = j + k * dx;
-        if (!grid[row]?.[col]) return false;
-        str += grid[row][col];
-      }
-      return str === "MAS";
-    };
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, -1],
+    [-1, 1],
+    [-1, -1],
+  ];
 
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] !== "X") continue;
+  const checkDirection = (i: number, j: number, dx: number, dy: number) =>
+    grid[i + dy]?.[j + dx] === "M" &&
+    grid[i + 2 * dy]?.[j + 2 * dx] === "A" &&
+    grid[i + 3 * dy]?.[j + 3 * dx] === "S";
 
+  const checkPattern = (i: number, j: number) => {
+    const tl = grid[i - 1]?.[j - 1];
+    const tr = grid[i - 1]?.[j + 1];
+    const bl = grid[i + 1]?.[j - 1];
+    const br = grid[i + 1]?.[j + 1];
+
+    return (
+      ((tl === "M" && br === "S") || (tl === "S" && br === "M")) &&
+      ((bl === "M" && tr === "S") || (bl === "S" && tr === "M"))
+    );
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === "X") {
         for (const [dx, dy] of directions) {
-          if (checkDirection(i, j, dx, dy)) count++;
+          if (checkDirection(i, j, dx, dy)) p1Count++;
         }
+      } else if (grid[i][j] === "A") {
+        if (checkPattern(i, j)) p2Count++;
       }
     }
-    return count;
-  };
-
-  const part2 = () => {
-    let count = 0;
-
-    const checkPattern = (i: number, j: number) => {
-      const tl = grid[i - 1]?.[j - 1];
-      const tr = grid[i - 1]?.[j + 1];
-      const bl = grid[i + 1]?.[j - 1];
-      const br = grid[i + 1]?.[j + 1];
-
-      return (
-        ((tl === "M" && br === "S") || (tl === "S" && br === "M")) &&
-        ((bl === "M" && tr === "S") || (bl === "S" && tr === "M"))
-      );
-    };
-
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] !== "A") continue;
-        if (checkPattern(i, j)) count++;
-      }
-    }
-    return count;
-  };
+  }
 
   return {
-    part1: part1(),
-    part2: part2(),
+    part1: p1Count,
+    part2: p2Count,
   };
 };
