570b84a99179ef767a4f056e9cdb5e3d1d670121
Fadi George <fadii925@gmail.com>
2024-12-03 19:23:53 -0800

    prep day4

---
+++ aoc/template.ts
@@ -0,0 +1,12 @@
+export default (input: string) => {
+  input.split("\n").forEach(() => {});
+
+  const part1 = () => {};
+
+  const part2 = () => {};
+
+  return {
+    part1: part1(),
+    part2: part2(),
+  };
+};
