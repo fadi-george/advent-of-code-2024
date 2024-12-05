ff6c7a148ebb3d55f40c0c42066d0f5d3cd41d7d
Fadi George <fadii925@gmail.com>
2024-12-04 23:53:04 -0800

    fix order of  getting stars if years have descending order

--- server/src/stars.ts
+++ server/src/stars.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import * as jsdom from "jsdom";
 
+const MAX_DAYS = 25;
+
 export const starsRoute = new Hono().get("/:year", async (c) => {
   const year = c.req.param("year");
 
@@ -12,16 +14,19 @@ export const starsRoute = new Hono().get("/:year", async (c) => {
   const text = await response.text();
   const { document } = new jsdom.JSDOM(text).window;
 
-  const stars = new Array(25).fill(0);
+  const stars = new Array(MAX_DAYS).fill(0);
   const dayEls = document.querySelectorAll('a[class^="calendar-day"]');
   dayEls.forEach((dayEl, i) => {
+    const dayIndex = dayEl.classList.toString().match(/\d+/)?.[0];
     let count = 0;
     if (dayEl.classList.contains("calendar-verycomplete")) {
       count = 2;
     } else if (dayEl.classList.contains("calendar-complete")) {
       count = 1;
     }
-    stars[i] = count;
+    if (dayIndex) {
+      stars[+dayIndex - 1] = count;
+    }
   });
 
   return c.json(stars);
