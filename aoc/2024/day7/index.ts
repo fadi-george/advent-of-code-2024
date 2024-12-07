460daadbc932dcdc6670e39876e7662b20685c14
Fadi George <fadii925@gmail.com>
2024-12-06 23:04:21 -0800

    optimize day 7

--- aoc/2024/day7/index.ts
+++ aoc/2024/day7/index.ts
@@ -1,36 +1,28 @@
-const getCombs2 = (v: number[], allowConcat = false) => {
-  const res: string[] = [];
-  if (v.length === 1) return [];
-  if (v.length === 2)
-    return [`${v[0]} + ${v[1]}`, `${v[0]} * ${v[1]}`].concat(
-      allowConcat ? [`${v[0]} || ${v[1]}`] : []
-    );
+const getCombs = (arr: number[], allowConcat = false) => {
+  if (arr.length === 1) return [arr[0]];
 
-  getCombs2(v.slice(1), allowConcat).forEach((strs) => {
-    res.push(`${v[0]} + ${strs}`);
-    res.push(`${v[0]} * ${strs}`);
-    if (allowConcat) res.push(`${v[0]} || ${strs}`);
-  });
+  const res: number[] = [];
+  const q = [
+    { v: arr[0] + arr[1], l: 2 },
+    { v: arr[0] * arr[1], l: 2 },
+    ...(allowConcat ? [{ v: +`${arr[0]}${arr[1]}`, l: 2 }] : []),
+  ];
+
+  for (const curr of q) {
+    if (curr.l === arr.length) {
+      res.push(curr.v);
+      continue;
+    }
+
+    q.push(
+      { v: curr.v + arr[curr.l], l: curr.l + 1 },
+      { v: curr.v * arr[curr.l], l: curr.l + 1 },
+      ...(allowConcat ? [{ v: +`${curr.v}${arr[curr.l]}`, l: curr.l + 1 }] : [])
+    );
+  }
 
   return res;
 };
-const evalCombs = (combs: string[]) =>
-  combs.map((comb) => {
-    const ops = comb.split(" ");
-
-    let res = +ops[0];
-    for (let i = 1; i < ops.length; i += 2) {
-      if (ops[i] === "+") {
-        res += +(+ops[i + 1]);
-      } else if (ops[i] === "*") {
-        res = res * +(+ops[i + 1]);
-      } else if (ops[i] === "||") {
-        res = +`${res}${ops[i + 1]}`;
-      }
-    }
-
-    return res;
-  });
 
 export default (input: string) => {
   const vals: number[][] = [];
@@ -42,16 +34,12 @@ export default (input: string) => {
   let p1 = 0;
   let p2 = 0;
   vals.forEach((val) => {
-    const product = val[0];
-    const combs = evalCombs(getCombs2(val.slice(1)));
-    const combs2 = evalCombs(getCombs2(val.slice(1), true));
-    if (combs.some((n) => n === product)) p1 += product;
-    if (combs2.some((n) => n === product)) {
-      p2 += product;
-    }
+    const [product, ...rest] = val;
+    const combs = getCombs(rest);
+    const combs2 = getCombs(rest, true);
+    p1 += combs.includes(product) ? product : 0;
+    p2 += combs2.includes(product) ? product : 0;
   });
 
   return { part1: p1, part2: p2 };
 };
-
-// 74493743489417 too low
