ee8b6517f333c96517e56b30821fcc35f83774f5
Fadi George <fadii925@gmail.com>
2024-12-06 22:24:47 -0800

    d7 p2

--- aoc/2024/day7/index.ts
+++ aoc/2024/day7/index.ts
@@ -10,9 +10,8 @@ export default (input: string) => {
   let p2 = 0;
   vals.forEach((val) => {
     const product = val[0];
-    const combs = getCombs(val.slice(1));
-    const combs2 = getCombs(val.slice(1), true);
-    // console.log(product, combs, combs2);
+    const combs = evalCombs(getCombs2(val.slice(1)));
+    const combs2 = evalCombs(getCombs2(val.slice(1), true));
     if (combs.some((n) => n === product)) p1 += product;
     if (combs2.some((n) => n === product)) p2 += product;
   });
@@ -20,63 +19,37 @@ export default (input: string) => {
   return { part1: p1, part2: p2 };
 };
 
-const getCombs = (val: number[], allowConcat = false) => {
-  // console.log(val);
-  if (val.length === 2) {
-    const res = [val[0] + val[1], val[0] * val[1]];
-    if (allowConcat) res.push(+`${val[0]}${val[1]}`);
-    return res;
-  } else if (val.length === 1) {
-    return val;
-  }
-  const res: number[] = [];
-  // const last = val[val.length - 1];
-  // const first = val[0];
-  // getCombs(val.slice(0, -1)).forEach((n) => {
-  //   res.push(n + last);
-  //   res.push(n * last);
-  //   if (allowConcat) res.push(+`${n}${last}`);
-  // });
-  // if (allowConcat) {
-  //   getCombs(val.slice(1), true).forEach((n) => {
-  //     res.push(+`${first}${n}`);
-  //     res.push(+`${n}${first}`);
-  //   });
-  // }
-  return res;
-};
+const getCombs2 = (v: number[], allowConcat = false) => {
+  const res: string[] = [];
+  if (v.length === 1) return [];
+  if (v.length === 2)
+    return [`${v[0]} + ${v[1]}`, `${v[0]} * ${v[1]}`].concat(
+      allowConcat ? [`${v[0]} || ${v[1]}`] : []
+    );
 
-const getCombs2 = (val: number[], allowConcat = false) => {
-  const res: unknown[] = [];
-  if (val.length === 1) return [];
-  if (val.length === 2)
-    return [
-      [val[0], "+", val[1]],
-      [val[0], "*", val[1]],
-    ].concat(allowConcat ? [[val[0], "||", val[1]]] : []);
-
-  getCombs2(val.slice(1), allowConcat).forEach((combs) => {
-    console.log({ v0: val[0], combs });
-    res.push([val[0], "+"].concat(combs));
-    res.push([val[0], "*"].concat(combs));
-    if (allowConcat) res.push([val[0], "||"].concat(combs));
+  getCombs2(v.slice(1), allowConcat).forEach((strs) => {
+    res.push(`${v[0]} + ${strs}`);
+    res.push(`${v[0]} * ${strs}`);
+    if (allowConcat) res.push(`${v[0]} || ${strs}`);
   });
 
   return res;
 };
-const evalCombs = (combs: [number, string, number][]) => {
-  return combs.map((comb) => {
-    for (let i = 1; i < comb.length; i += 2) {
-      const op = comb[i];
-      const a: number = comb[i - 1];
-      const b: number = comb[i + 1];
-      if (op === "+") return a + b;
-      if (op === "*") return a * b;
-      if (op === "||") return +`${a}${b}`;
-    }
+const evalCombs = (combs: string[]) =>
+  combs.map((comb) => {
+    const strs = comb.split(" || ");
+    const nums = strs.map((str) => {
+      if (str.length === 1) return +str;
+      const strr = str.split(" ");
+      let res = +strr[0];
+      for (let i = 1; i < strr.length; i += 2) {
+        if (strr[i] === "+") {
+          res += +(+strr[i + 1]);
+        } else if (strr[i] === "*") {
+          res = res * +strr[i + 1];
+        }
+      }
+      return res;
+    });
+    return +`${nums.join("")}`;
   });
-};
-
-// console.log(getCombs([1, 2, 3]));
-// console.log(getCombs([11, 6, 16, 20]));
-console.log(getCombs2([1, 2, 3], true));
