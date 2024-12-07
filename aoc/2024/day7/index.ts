556e63204781b974315783ab3ac5b5446b812441
Fadi George <fadii925@gmail.com>
2024-12-06 21:57:38 -0800

    wip

--- aoc/2024/day7/index.ts
+++ aoc/2024/day7/index.ts
@@ -30,21 +30,53 @@ const getCombs = (val: number[], allowConcat = false) => {
     return val;
   }
   const res: number[] = [];
-  const last = val[val.length - 1];
-  const first = val[0];
-  getCombs(val.slice(0, -1)).forEach((n) => {
-    res.push(n + last);
-    res.push(n * last);
-    if (allowConcat) res.push(+`${n}${last}`);
-  });
-  if (allowConcat) {
-    getCombs(val.slice(1), true).forEach((n) => {
-      res.push(+`${first}${n}`);
-      res.push(+`${n}${first}`);
-    });
-  }
+  // const last = val[val.length - 1];
+  // const first = val[0];
+  // getCombs(val.slice(0, -1)).forEach((n) => {
+  //   res.push(n + last);
+  //   res.push(n * last);
+  //   if (allowConcat) res.push(+`${n}${last}`);
+  // });
+  // if (allowConcat) {
+  //   getCombs(val.slice(1), true).forEach((n) => {
+  //     res.push(+`${first}${n}`);
+  //     res.push(+`${n}${first}`);
+  //   });
+  // }
   return res;
 };
 
+const getCombs2 = (val: number[], allowConcat = false) => {
+  const res: unknown[] = [];
+  if (val.length === 1) return [];
+  if (val.length === 2)
+    return [
+      [val[0], "+", val[1]],
+      [val[0], "*", val[1]],
+    ].concat(allowConcat ? [[val[0], "||", val[1]]] : []);
+
+  getCombs2(val.slice(1), allowConcat).forEach((combs) => {
+    console.log({ v0: val[0], combs });
+    res.push([val[0], "+"].concat(combs));
+    res.push([val[0], "*"].concat(combs));
+    if (allowConcat) res.push([val[0], "||"].concat(combs));
+  });
+
+  return res;
+};
+const evalCombs = (combs: [number, string, number][]) => {
+  return combs.map((comb) => {
+    for (let i = 1; i < comb.length; i += 2) {
+      const op = comb[i];
+      const a: number = comb[i - 1];
+      const b: number = comb[i + 1];
+      if (op === "+") return a + b;
+      if (op === "*") return a * b;
+      if (op === "||") return +`${a}${b}`;
+    }
+  });
+};
+
 // console.log(getCombs([1, 2, 3]));
-console.log(getCombs([11, 6, 16, 20]));
+// console.log(getCombs([11, 6, 16, 20]));
+console.log(getCombs2([1, 2, 3], true));
