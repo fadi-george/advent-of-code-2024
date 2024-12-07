4de93da30706f55b17765a5f21ae3a27f087539c
Fadi George <fadii925@gmail.com>
2024-12-06 22:53:24 -0800

    add day 7 tests

--- aoc/2024/day7/day.test.ts
+++ aoc/2024/day7/day.test.ts
@@ -6,14 +6,14 @@ describe("day 7", () => {
   test("example input", () => {
     const input = readFile("aoc/2024/day7/sample.txt");
     const result = solution(input);
-    // expect(result.part1).toBe(41);
-    // expect(result.part2).toBe(6);
+    expect(result.part1).toBe(3749);
+    expect(result.part2).toBe(11387);
   });
 
   test("puzzle input", () => {
     const input = readFile("aoc/2024/day7/input.txt");
     const result = solution(input);
-    // expect(result.part1).toBe(4883);
-    // expect(result.part2).toBe(1655);
+    expect(result.part1).toBe(20281182715321);
+    expect(result.part2).toBe(159490400628354);
   });
 });
