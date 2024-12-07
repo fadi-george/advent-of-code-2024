0ad72795fd17f53ef638535fc6cbc9d24eab0ef6
Fadi George <fadii925@gmail.com>
2024-12-06 18:57:55 -0800

    2024 d1 python clean up

--- aoc/2024/day1/main.py
+++ aoc/2024/day1/main.py
@@ -1,34 +1,5 @@
-import os
+import fileinput
 
-# Get the root directory of the project
-root_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the absolute path to the file
-file_path = os.path.join(root_dir, "input.txt")
-
-left = []
-right = []
-with open(file_path, "r") as file:
-    lines = [x.strip() for x in file.readlines()]
-    for line in lines:
-        a, b = map(int, line.split())
-        left.append(a)
-        right.append(b)
-
-# Part 1
-# Calculate difference between left and right arrays
-left.sort()
-right.sort()
-dist = sum(abs(r - l) for l, r in zip(left, right))
-
-print("Part 1: ", dist)
-
-
-# Part 2
-# Calculate score based on frequency of each number
-freq = {}
-for r in right:
-    freq[r] = freq.get(r, 0) + 1
-
-score = sum(l * freq.get(l, 0) for l in left)
-print("Part 2: ", score)
+left, right = zip(*[map(int, line.split()) for line in fileinput.input()])
+print(sum(abs(r - l) for l, r in zip(sorted(left), sorted(right))))
+print(sum(l * right.count(l) for l in left))
