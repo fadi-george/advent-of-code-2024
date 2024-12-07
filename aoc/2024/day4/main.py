0bd59da799a914bd2f753e9aaee53e63f1138aa5
Fadi George <fadii925@gmail.com>
2024-12-07 12:43:49 -0800

    shrink day4 python

--- aoc/2024/day4/main.py
+++ aoc/2024/day4/main.py
@@ -1,38 +1,22 @@
 import fileinput
 
 grid = [list(line.strip()) for line in fileinput.input()]
-w, h = len(grid[0]), len(grid)
+
 dirs = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+w, h = len(grid[0]), len(grid)
+wr, hr = range(w), range(h)
 
-get = lambda i, j: None if i < 0 or j < 0 or i >= h or j >= w else grid[i][j]
+get = lambda i, j: "" if i < 0 or j < 0 or i >= h or j >= w else grid[i][j]
 check_dir = (
-    lambda i, j, dx, dy: get(i + dy, j + dx) == "M"
-    and get(i + 2 * dy, j + 2 * dx) == "A"
-    and get(i + 3 * dy, j + 3 * dx) == "S"
+    lambda i, j, dx, dy: "".join(get(i + dy * d, j + dx * d) for d in range(4))
+    == "XMAS"
+)
+check_pat = lambda i, j: (
+    "".join(get(i + dy, j + dx) for dx, dy in [(-1, -1), (0, 0), (1, 1)])
+    in ["MAS", "SAM"]
+    and "".join(get(i + dy, j + dx) for dx, dy in [(-1, 1), (0, 0), (1, -1)])
+    in ["MAS", "SAM"]
 )
 
-check_pattern = lambda i, j: (
-    (
-        (get(i - 1, j - 1) == "M" and get(i + 1, j + 1) == "S")
-        or (get(i - 1, j - 1) == "S" and get(i + 1, j + 1) == "M")
-    )
-    and (
-        (get(i - 1, j + 1) == "M" and get(i + 1, j - 1) == "S")
-        or (get(i - 1, j + 1) == "S" and get(i + 1, j - 1) == "M")
-    )
-)
-
-print(
-    sum(
-        [
-            check_dir(i, j, dx, dy)
-            for i in range(h)
-            for j in range(w)
-            for dx, dy in dirs
-            if grid[i][j] == "X"
-        ]
-    )
-)
-print(
-    sum([check_pattern(i, j) for i in range(h) for j in range(w) if grid[i][j] == "A"])
-)
+print(sum([check_dir(i, j, dx, dy) for i in hr for j in wr for dx, dy in dirs]))
+print(sum([check_pat(i, j) for i in hr for j in wr]))
