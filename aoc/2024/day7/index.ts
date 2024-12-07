9b42a588f8d1b15bc8c5dc73c276dd15ba748620
Fadi George <fadii925@gmail.com>
2024-12-06 22:37:41 -0800

    day7 p2 cursed

--- aoc/2024/day7/index.ts
+++ aoc/2024/day7/index.ts
@@ -1,24 +1,3 @@
-const OPS = ["*", "+"];
-export default (input: string) => {
-  const vals: number[][] = [];
-  input.split("\n").forEach((line) => {
-    const nums = line.match(/\d+/g)!.map(Number);
-    vals.push(nums);
-  });
-
-  let p1 = 0;
-  let p2 = 0;
-  vals.forEach((val) => {
-    const product = val[0];
-    const combs = evalCombs(getCombs2(val.slice(1)));
-    const combs2 = evalCombs(getCombs2(val.slice(1), true));
-    if (combs.some((n) => n === product)) p1 += product;
-    if (combs2.some((n) => n === product)) p2 += product;
-  });
-
-  return { part1: p1, part2: p2 };
-};
-
 const getCombs2 = (v: number[], allowConcat = false) => {
   const res: string[] = [];
   if (v.length === 1) return [];
@@ -37,19 +16,42 @@ const getCombs2 = (v: number[], allowConcat = false) => {
 };
 const evalCombs = (combs: string[]) =>
   combs.map((comb) => {
-    const strs = comb.split(" || ");
-    const nums = strs.map((str) => {
-      if (str.length === 1) return +str;
-      const strr = str.split(" ");
-      let res = +strr[0];
-      for (let i = 1; i < strr.length; i += 2) {
-        if (strr[i] === "+") {
-          res += +(+strr[i + 1]);
-        } else if (strr[i] === "*") {
-          res = res * +strr[i + 1];
-        }
+    const ops = comb.split(" ");
+
+    let res = +ops[0];
+    for (let i = 1; i < ops.length; i += 2) {
+      if (ops[i] === "+") {
+        res += +(+ops[i + 1]);
+      } else if (ops[i] === "*") {
+        res = res * +(+ops[i + 1]);
+      } else if (ops[i] === "||") {
+        res = +`${res}${ops[i + 1]}`;
       }
-      return res;
-    });
-    return +`${nums.join("")}`;
+    }
+
+    return res;
   });
+
+export default (input: string) => {
+  const vals: number[][] = [];
+  input.split("\n").forEach((line) => {
+    const nums = line.match(/\d+/g)!.map(Number);
+    vals.push(nums);
+  });
+
+  let p1 = 0;
+  let p2 = 0;
+  vals.forEach((val) => {
+    const product = val[0];
+    const combs = evalCombs(getCombs2(val.slice(1)));
+    const combs2 = evalCombs(getCombs2(val.slice(1), true));
+    if (combs.some((n) => n === product)) p1 += product;
+    if (combs2.some((n) => n === product)) {
+      p2 += product;
+    }
+  });
+
+  return { part1: p1, part2: p2 };
+};
+
+// 74493743489417 too low
