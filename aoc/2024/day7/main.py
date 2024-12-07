1bd29f20b1d4c3bafd27fd949ce27971ea986a98
Fadi George <fadii925@gmail.com>
2024-12-07 00:28:00 -0800

    day7 python

---
+++ aoc/2024/day7/main.py
@@ -0,0 +1,31 @@
+import fileinput
+import re
+from math import log10, floor
+
+
+def number_concat(a: int, b: int) -> int:
+    return a * 10 ** (floor(log10(b)) + 1) + b
+
+
+def check_target(arr: list[int], target: int, allow_concat: bool = False) -> bool:
+    def dfs(value: int, length: int) -> bool:
+        if length == len(arr):
+            return value == target
+        return (
+            dfs(value + arr[length], length + 1)
+            or dfs(value * arr[length], length + 1)
+            or (allow_concat and dfs(number_concat(value, arr[length]), length + 1))
+        )
+
+    return (
+        dfs(arr[0] + arr[1], 2)
+        or dfs(arr[0] * arr[1], 2)
+        or (allow_concat and dfs(number_concat(arr[0], arr[1]), 2))
+    )
+
+
+vals = [[int(n) for n in re.findall(r"\d+", line)] for line in fileinput.input()]
+p1 = sum(product for product, *rest in vals if check_target(rest, product))
+p2 = sum(product for product, *rest in vals if check_target(rest, product, True))
+print(p1)
+print(p2)
