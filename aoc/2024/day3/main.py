e1b00d294a356f715468cfef17448a0fac8ac91e
Fadi George <fadii925@gmail.com>
2024-12-06 20:13:58 -0800

    shrink python day 3

--- aoc/2024/day3/main.py
+++ aoc/2024/day3/main.py
@@ -1,35 +1,22 @@
 import fileinput
 import re
 
-p1_vals = []
-p2_vals = []
-
+p1 = p2 = 0
 ignored = False
 
 for line in fileinput.input():
-    matches = re.finditer(r"mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)", line)
-
+    matches = re.finditer(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)", line)
     for match in matches:
         command = match.group(0)
-        x, y = match.group(1, 2) if match.group(1) else (None, None)
-
         if command == "do()":
             ignored = False
         elif command == "don't()":
             ignored = True
         else:
-            p1_vals.append([int(x), int(y)])
-            if not ignored:
-                p2_vals.append([int(x), int(y)])
+            x, y = map(int, match.groups())
+            product = x * y
+            p1 += product
+            p2 += product * (not ignored)
 
-
-def part1():
-    return sum(x * y for x, y in p1_vals)
-
-
-def part2():
-    return sum(x * y for x, y in p2_vals)
-
-
-print(part1())
-print(part2())
+print(p1)
+print(p2)
