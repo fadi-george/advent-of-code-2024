1f50c70f4509909086fd3e80c041020fda2ca55a
Fadi George <fadii925@gmail.com>
2024-12-06 23:55:23 -0800

    day 7 optimization dfs

--- aoc/2024/day7/index.ts
+++ aoc/2024/day7/index.ts
@@ -2,44 +2,26 @@ const numberConcat = (a: number, b: number) =>
   a * 10 ** (Math.floor(Math.log10(b)) + 1) + b;
 
 const checkTarget = (arr: number[], target: number, allowConcat = false) => {
-  if (arr.length === 1) return arr[0] === target;
+  const dfs = (value: number, length: number): boolean =>
+    length === arr.length
+      ? value === target
+      : dfs(value + arr[length], length + 1) ||
+        dfs(value * arr[length], length + 1) ||
+        (allowConcat && dfs(numberConcat(value, arr[length]), length + 1));
 
-  const q = [
-    { v: arr[0] + arr[1], l: 2 },
-    { v: arr[0] * arr[1], l: 2 },
-    ...(allowConcat ? [{ v: numberConcat(arr[0], arr[1]), l: 2 }] : []),
-  ];
-
-  for (const curr of q) {
-    if (curr.v === target && curr.l === arr.length) return true;
-    if (curr.l === arr.length) continue;
-
-    q.push(
-      { v: curr.v + arr[curr.l], l: curr.l + 1 },
-      { v: curr.v * arr[curr.l], l: curr.l + 1 },
-      ...(allowConcat
-        ? [{ v: numberConcat(curr.v, arr[curr.l]), l: curr.l + 1 }]
-        : [])
-    );
-  }
-
-  return false;
+  return (
+    dfs(arr[0] + arr[1], 2) ||
+    dfs(arr[0] * arr[1], 2) ||
+    (allowConcat && dfs(numberConcat(arr[0], arr[1]), 2))
+  );
 };
 
 export default (input: string) => {
-  const vals: number[][] = [];
-  input.split("\n").forEach((line) => {
-    const nums = line.match(/\d+/g)!.map(Number);
-    vals.push(nums);
-  });
-
-  let p1 = 0;
-  let p2 = 0;
-  vals.forEach((val) => {
-    const [product, ...rest] = val;
+  const vals = input.split("\n").map((line) => line.match(/\d+/g)!.map(Number));
+  let [p1, p2] = [0, 0];
+  vals.forEach(([product, ...rest]) => {
     p1 += checkTarget(rest, product) ? product : 0;
     p2 += checkTarget(rest, product, true) ? product : 0;
   });
-
   return { part1: p1, part2: p2 };
 };
