523503794731716044b56a14b32da0012d4e6bc2
Fadi George <fadii925@gmail.com>
2024-12-07 21:40:08 -0800

    cursed day8 p2

--- aoc/2024/day8/index.ts
+++ aoc/2024/day8/index.ts
@@ -11,54 +11,79 @@ export default (input: string) => {
     for (let j = 0; j < w; j++) {
       const c = grid[i][j];
       if (c === ".") continue;
+      if (c === "#") continue;
       antennas[c] = antennas[c] || [];
       antennas[c].push([i, j]);
     }
   }
 
-  let visited: Set<string> = new Set();
+  const getImpact = (isCapped: boolean) => {
+    console.clear();
+    let visited: Set<string> = new Set();
 
-  console.log(antennas);
-  // const fills: Record<string, [number, number][]> = {};
-  Object.keys(antennas).forEach((a) => {
-    const ps = antennas[a];
-    for (let i = 0; i < ps.length; i++) {
-      for (let j = i + 1; j < ps.length; j++) {
-        const [x1, y1] = ps[i];
-        const [x2, y2] = ps[j];
-        // console.log({ x1, y1, x2, y2 });
-        const dx = x2 - x1;
-        const dy = y2 - y1;
+    console.log(antennas);
+    // const fills: Record<string, [number, number][]> = {};
+    Object.keys(antennas).forEach((a) => {
+      const ps = antennas[a];
+      for (let i = 0; i < ps.length; i++) {
+        for (let j = i + 1; j < ps.length; j++) {
+          const [x1, y1] = ps[i];
+          const [x2, y2] = ps[j];
+          // console.log({ x1, y1, x2, y2 });
+          const dx = x2 - x1;
+          const dy = y2 - y1;
 
-        const p1 = [x1 - dx, y1 - dy];
-        const p2 = [x2 + dx, y2 + dy];
-        // console.log({ dx, dy, p1, p2 });
-        if (grid[p1[0]]?.[p1[1]] !== undefined) {
-          // console.log(p1, get1DIndex(p1[0], p1[1], w));
-          visited.add(`${p1[0]},${p1[1]}`);
-          // visited.add(get1DIndex(p1[0], p1[1], w));
-          // grid[p1[0]] =
+          let p1 = [x1 - dx, y1 - dy];
+          let p2 = [x2 + dx, y2 + dy];
+
+          if (!isCapped) {
+            visited.add(`${x1},${y1}`);
+            visited.add(`${x2},${y2}`);
+          }
+
+          while (grid[p1[0]]?.[p1[1]] !== undefined) {
+            visited.add(`${p1[0]},${p1[1]}`);
+            p1 = [p1[0] - dx, p1[1] - dy];
+            if (isCapped) break;
+          }
+          while (grid[p2[0]]?.[p2[1]] !== undefined) {
+            visited.add(`${p2[0]},${p2[1]}`);
+            p2 = [p2[0] + dx, p2[1] + dy];
+            if (isCapped) break;
+          }
+
+          // // if (isCapped) {
+          // // console.log({ dx, dy, p1, p2 });
+          // if (grid[p1[0]]?.[p1[1]] !== undefined) {
+          //   // console.log(p1, get1DIndex(p1[0], p1[1], w));
+          //   visited.add(`${p1[0]},${p1[1]}`);
+          //   // visited.add(get1DIndex(p1[0], p1[1], w));
+          //   // grid[p1[0]] =
+          // }
+          // if (grid[p2[0]]?.[p2[1]] !== undefined) {
+          //   // console.log(p2, get1DIndex(p2[0], p2[1], w));
+          //   // visited.add(get1DIndex(p2[0], p2[1], w));
+          //   visited.add(`${p2[0]},${p2[1]}`);
+          // }
+          // const idx = get1DIndex(w, x1 + dx, y1 + dy);
+          // visited.add(idx);
+          // fills[a] = fills[a] || [];
+
+          // fills[a].push([x1 + dx, y1 + dy]);
+          // fills[a].push([x1 - dx, y1 - dy]);
         }
-        if (grid[p2[0]]?.[p2[1]] !== undefined) {
-          // console.log(p2, get1DIndex(p2[0], p2[1], w));
-          // visited.add(get1DIndex(p2[0], p2[1], w));
-          visited.add(`${p2[0]},${p2[1]}`);
-        }
-        // const idx = get1DIndex(w, x1 + dx, y1 + dy);
-        // visited.add(idx);
-        // fills[a] = fills[a] || [];
-
-        // fills[a].push([x1 + dx, y1 + dy]);
-        // fills[a].push([x1 - dx, y1 - dy]);
       }
-    }
-  });
+    });
+    // console.log(visited);
+    return visited.size;
+  };
 
   // console.log(fills);
   // console.log(visited);
   // console.log(visited.size);
 
-  let p1 = 0;
-  let p2 = 0;
-  return { part1: visited.size, part2: p2 };
+  return {
+    part1: getImpact(true),
+    part2: getImpact(false),
+  };
 };
