bca890dff55c755856d7defbeaee44af667e3bba
Fadi George <fadii925@gmail.com>
2024-12-07 22:14:48 -0800

    day8 python clean up

--- aoc/2024/day8/main.py
+++ aoc/2024/day8/main.py
@@ -1,30 +1,43 @@
 import fileinput
-import re
+
+grid = [line.strip() for line in fileinput.input()]
+w, h = len(grid[0]), len(grid)
+
+ants = {
+    char: [(i, j) for i in range(h) for j in range(w) if grid[i][j] == char]
+    for char in set(char for row in grid for char in row if char not in ".#")
+}
 
 
-def check_target(arr: list[int], target: int, allow_concat: bool = False) -> bool:
-    def dfs(value: int, i: int) -> bool:
-        if i == 0:
-            return value == arr[0]
+def get_impact(is_capped):
+    visited = set()
 
-        d = 1
-        n = arr[i]
-        while d <= n:
-            d *= 10
+    def visit_points(p, direction):
+        while 0 <= p[0] < h and 0 <= p[1] < w:
+            visited.add((p[0], p[1]))
+            p = [p[0] + direction[0], p[1] + direction[1]]
+            if is_capped:
+                break
 
-        # Check concatenation
-        if allow_concat and value % d == n:
-            if dfs(value // d, i - 1):
-                return True
-        # Check division
-        if value % n == 0 and dfs(value // n, i - 1):
-            return True
-        # Check subtraction
-        return dfs(value - n, i - 1)
+    for points in ants.values():
+        pairs = [
+            (points[i], points[j])
+            for i in range(len(points))
+            for j in range(i + 1, len(points))
+        ]
 
-    return dfs(target, len(arr) - 1)
+        for (x1, y1), (x2, y2) in pairs:
+            dx, dy = x2 - x1, y2 - y1
+            p1, p2 = [x1 - dx, y1 - dy], [x2 + dx, y2 + dy]
+
+            if not is_capped:
+                visited.update([(x1, y1), (x2, y2)])
+
+            visit_points(p1, [-dx, -dy])
+            visit_points(p2, [dx, dy])
+
+    return len(visited)
 
 
-vals = [[int(n) for n in re.findall(r"\d+", line)] for line in fileinput.input()]
-print(sum(p for p, *r in vals if check_target(r, p)))
-print(sum(p for p, *r in vals if check_target(r, p, True)))
+print(get_impact(True))
+print(get_impact(False))
