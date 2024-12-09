c5d6cea9dad28610c6da39cd89c9580620eb986c
Fadi George <fadii925@gmail.com>
2024-12-08 20:53:57 -0800

    day9 prep

--- aoc/2024/day8/main.py
+++ aoc/2024/day8/main.py
@@ -1,39 +1,39 @@
-import fileinput
-from itertools import combinations
+# import fileinput
+# from itertools import combinations
 
-grid = [line.strip() for line in fileinput.input()]
-w, h = len(grid[0]), len(grid)
+# grid = [line.strip() for line in fileinput.input()]
+# w, h = len(grid[0]), len(grid)
 
-ants = {
-    char: [(i, j) for i in range(h) for j in range(w) if grid[i][j] == char]
-    for char in set(char for row in grid for char in row if char not in ".#")
-}
-
-
-def get_impact(is_capped):
-    visited = set()
-
-    def visit_points(p, direction):
-        while 0 <= p[0] < h and 0 <= p[1] < w:
-            visited.add((p[0], p[1]))
-            p = [p[0] + direction[0], p[1] + direction[1]]
-            if is_capped:
-                break
-
-    for points in ants.values():
-        pairs = combinations(points, 2)
-        for (x1, y1), (x2, y2) in pairs:
-            dx, dy = x2 - x1, y2 - y1
-            p1, p2 = [x1 - dx, y1 - dy], [x2 + dx, y2 + dy]
-
-            if not is_capped:
-                visited.update([(x1, y1), (x2, y2)])
-
-            visit_points(p1, [-dx, -dy])
-            visit_points(p2, [dx, dy])
-
-    return len(visited)
+# ants = {
+#     char: [(i, j) for i in range(h) for j in range(w) if grid[i][j] == char]
+#     for char in set(char for row in grid for char in row if char not in ".#")
+# }
 
 
-print(get_impact(True))
-print(get_impact(False))
+# def get_impact(is_capped):
+#     visited = set()
+
+#     def visit_points(p, direction):
+#         while 0 <= p[0] < h and 0 <= p[1] < w:
+#             visited.add((p[0], p[1]))
+#             p = [p[0] + direction[0], p[1] + direction[1]]
+#             if is_capped:
+#                 break
+
+#     for points in ants.values():
+#         pairs = combinations(points, 2)
+#         for (x1, y1), (x2, y2) in pairs:
+#             dx, dy = x2 - x1, y2 - y1
+#             p1, p2 = [x1 - dx, y1 - dy], [x2 + dx, y2 + dy]
+
+#             if not is_capped:
+#                 visited.update([(x1, y1), (x2, y2)])
+
+#             visit_points(p1, [-dx, -dy])
+#             visit_points(p2, [dx, dy])
+
+#     return len(visited)
+
+
+# print(get_impact(True))
+# print(get_impact(False))
---
+++ aoc/2024/day9/index.ts
@@ -0,0 +1,5 @@
+export default (input: string) => {
+  const grid = input.split("\n");
+
+  return { part1: "TODO", part2: "TODO" };
+};
