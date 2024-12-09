bdddf34b8b0f11ef4e163ca454f835f64b3e25ce
Fadi George <fadii925@gmail.com>
2024-12-08 22:21:58 -0800

    d8p2 wip

--- aoc/2024/day9/index.ts
+++ aoc/2024/day9/index.ts
@@ -1,45 +1,62 @@
 export default (input: string) => {
   const S = [];
-  // const fullStr = input;
-  let str = "";
+  let S2 = [];
   let id = 0;
   for (let i = 0; i < input.length; i++) {
     const amount = +input[i];
     if (i % 2 === 0) {
       S.push(...Array(amount).fill(id));
-      // str += `${i / 2}`.repeat(amount);
+      S2.push(`${id}`.repeat(amount));
       id++;
       if (amount === 0) continue;
     } else {
       S.push(...Array(amount).fill("."));
-      // str += ".".repeat(amount);
+      S2.push(".".repeat(amount));
     }
   }
 
-  // let S = str.split("");
-  let i = 0;
-  let j = S.length - 1;
-  while (i < j) {
-    if (S[i] !== ".") {
-      i++;
-      continue;
-    }
-    if (S[j] === ".") {
-      j--;
-      continue;
-    }
-    [S[i], S[j]] = [S[j], S[i]];
-  }
+  console.log(S);
+  console.log(S2);
+
+  console.log(S2.join(""));
+  console.log(S2);
 
   // let p1 = 0;
   // for (let i = 0; i < S.length; i++) {
   //   if (S[i] === ".") continue;
   //   p1 += +S[i] * i;
   // }
-  const p1 = S.filter((c) => c !== ".").reduce(
-    (acc, curr, i) => acc + +curr * i,
-    0
-  );
+  const p1 = updateChecksum(S)
+    .filter((c) => c !== ".")
+    .reduce((acc, curr, i) => acc + +curr * i, 0);
+
+  let i = 0;
+  let j = S2.length - 1;
+  while (i < j) {
+    if (!S2[i].startsWith(".")) {
+      i++;
+      continue;
+    }
+    if (S2[j].startsWith(".")) {
+      j--;
+      continue;
+    }
+    if (S2[j].length <= S2[i].length) {
+      [S2[i], S2[j]] = [S2[j], S2[i]];
+      i++;
+      j--;
+    } else {
+      j--;
+    }
+  }
+
+  console.log([...S2.join("")].join(""));
+  S2 = updateChecksum([...S2.join("")]);
+  console.log(S2.join(""));
+  const p2 = S2.reduce((acc, curr, i) => {
+    if (curr === ".") return acc;
+    return acc + +curr * i;
+  }, 0);
 
   // const p1 = S.reduce((acc, curr, i) => {
   //   if (curr === ".") return acc;
@@ -47,7 +64,25 @@ export default (input: string) => {
   // }, 0);
   // console.log(p1);
 
-  return { part1: p1, part2: "TODO" };
+  return { part1: p1, part2: p2 };
+};
+
+const updateChecksum = (str: string[]) => {
+  let i = 0;
+  let j = str.length - 1;
+  while (i < j) {
+    if (str[i] !== ".") {
+      i++;
+      continue;
+    }
+    if (str[j] === ".") {
+      j--;
+      continue;
+    }
+    [str[i], str[j]] = [str[j], str[i]];
+  }
+
+  return str;
 };
 
 // 88957760693 - wrong, wrong
