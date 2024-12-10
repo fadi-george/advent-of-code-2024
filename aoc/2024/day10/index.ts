e537eff1850216b73856785aa6659e35dbed877e
Fadi George <fadii925@gmail.com>
2024-12-09 23:46:58 -0800

    day10 slight optimization by using number

--- aoc/2024/day10/index.ts
+++ aoc/2024/day10/index.ts
@@ -3,13 +3,12 @@ import { DIRS } from "../../constants";
 const END_HEIGHT = 9;
 
 export default (input: string) => {
-  const grid = input.split("\n");
+  const grid = input.split("\n").map((r) => r.split("").map(Number));
 
   // get all start positions (height 0)
   const startPos = [];
   for (let r = 0; r < grid.length; r++)
-    for (let c = 0; c < grid[r].length; c++)
-      if (grid[r][c] === "0") startPos.push([r, c]);
+    for (let c = 0; c < grid[r].length; c++) if (grid[r][c] === 0) startPos.push([r, c]);
 
   // explore all trails from all start positions
   const q = startPos.map(([r, c]) => ({ sr: r, sc: c, r, c }));
@@ -18,7 +17,7 @@ export default (input: string) => {
 
   while (q.length > 0) {
     const { r, c, sr, sc } = q.shift()!;
-    const height = +grid[r][c];
+    const height = grid[r][c];
 
     if (height === END_HEIGHT) {
       // part 1 - must be a unique trail from a different start positions
@@ -30,8 +29,7 @@ export default (input: string) => {
 
     // go up, right, down, left
     for (const [dr, dc] of DIRS)
-      if (grid[r + dr]?.[c + dc] === `${height + 1}`)
-        q.push({ sr, sc, r: r + dr, c: c + dc });
+      if (grid[r + dr]?.[c + dc] === height + 1) q.push({ sr, sc, r: r + dr, c: c + dc });
   }
 
   return { part1: trailSet.size, part2: p2 };
