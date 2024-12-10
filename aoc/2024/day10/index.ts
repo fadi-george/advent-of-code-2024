80102d225811ca21a6301a2eb61b36f772d24816
Fadi George <fadii925@gmail.com>
2024-12-10 00:10:54 -0800

    optimization via dfs

--- aoc/2024/day10/index.ts
+++ aoc/2024/day10/index.ts
@@ -1,36 +1,41 @@
 import { DIRS } from "../../constants";
 
-const END_HEIGHT = 9;
+const [START_HEIGHT, END_HEIGHT] = [0, 9];
 
 export default (input: string) => {
-  const grid = input.split("\n").map((r) => r.split("").map(Number));
+  const grid = input
+    .split("\n")
+    .map((r) => r.split("").map((c) => (c === "." ? -1 : Number(c))));
 
   // get all start positions (height 0)
   const startPos = [];
   for (let r = 0; r < grid.length; r++)
-    for (let c = 0; c < grid[r].length; c++) if (grid[r][c] === 0) startPos.push([r, c]);
+    for (let c = 0; c < grid[r].length; c++)
+      if (grid[r][c] === START_HEIGHT) startPos.push([r, c]);
 
-  // explore all trails from all start positions
-  const q = startPos.map(([r, c]) => ({ sr: r, sc: c, r, c }));
-  const trailSet = new Set<string>();
-  let p2 = 0;
+  const uniquePaths = new Set<string>();
+  let paths = 0;
 
-  while (q.length > 0) {
-    const { r, c, sr, sc } = q.shift()!;
+  const dfs = (r: number, c: number, sr: number, sc: number) => {
     const height = grid[r][c];
 
     if (height === END_HEIGHT) {
       // part 1 - must be a unique trail from a different start positions
       // part 2 - start position does not matter
-      trailSet.add(`${r},${c},${sr},${sc}`);
-      p2 += 1;
-      continue;
+      uniquePaths.add(`${sr},${sc},${r},${c}`);
+      paths++;
+      return;
     }
 
     // go up, right, down, left
     for (const [dr, dc] of DIRS)
-      if (grid[r + dr]?.[c + dc] === height + 1) q.push({ sr, sc, r: r + dr, c: c + dc });
-  }
+      if (grid[r + dr]?.[c + dc] === height + 1) dfs(r + dr, c + dc, sr, sc);
+  };
 
-  return { part1: trailSet.size, part2: p2 };
+  for (const [sr, sc] of startPos) dfs(sr, sc, sr, sc);
+
+  return {
+    part1: uniquePaths.size,
+    part2: paths,
+  };
 };
