8ad104d886f68694bb2224f80386a291394bfd41
Fadi George <fadii925@gmail.com>
2024-12-09 20:59:35 -0800

    day10 wip

---
+++ aoc/day10/day.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "bun:test";
+import { readFile } from "../../lib/file";
+import solution from "./index";
+
+describe("day 9", () => {
+  test("example input", () => {
+    const input = readFile("aoc/2024/day9/sample.txt");
+    const result = solution(input);
+    expect(result.part1).toBe(1928);
+    expect(result.part2).toBe(2858);
+  });
+
+  test("puzzle input", () => {
+    const input = readFile("aoc/2024/day9/input.txt");
+    const result = solution(input);
+    expect(result.part1).toBe(6259790630969);
+    expect(result.part2).toBe(6289564433984);
+  });
+});
