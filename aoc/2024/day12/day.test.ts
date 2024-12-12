3673175064dc9aa99c4762df8f1a17841c625b3a
Fadi George <fadii925@gmail.com>
2024-12-12 00:42:38 -0800

    p2 wip

--- aoc/2024/day12/day.test.ts
+++ aoc/2024/day12/day.test.ts
@@ -7,6 +7,7 @@ describe("day 12", () => {
     const input = readFile("aoc/2024/day12/sample.txt");
     const result = solution(input);
     expect(result.part1).toBe(140);
+    expect(result.part2).toBe(80);
   });
 
   test("example input 2", () => {
@@ -21,10 +22,22 @@ describe("day 12", () => {
     expect(result.part1).toBe(1930);
   });
 
-  // test("puzzle input", () => {
-  //   const input = readFile("aoc/2024/day12/input.txt");
-  //   const result = solution(input);
-  //   expect(result.part1).toBe("");
-  //   expect(result.part2).toBe("");
-  // });
+  test("example input 4", () => {
+    const input = readFile("aoc/2024/day12/sample4.txt");
+    const result = solution(input);
+    expect(result.part2).toBe(236);
+  });
+
+  test("example input 5", () => {
+    const input = readFile("aoc/2024/day12/sample5.txt");
+    const result = solution(input);
+    expect(result.part2).toBe(368);
+  });
+
+  test("puzzle input", () => {
+    const input = readFile("aoc/2024/day12/input.txt");
+    const result = solution(input);
+    expect(result.part1).toBe(1375574);
+    // expect(result.part2).toBe(1375574);
+  });
 });
--- aoc/2024/day12/index.ts
+++ aoc/2024/day12/index.ts
@@ -5,26 +5,76 @@ export default (input: string) => {
   const plots = getPlotInfo(grid);
   console.table(grid);
   console.log(plots);
-  const price = getPrice(plots);
-
   return {
-    part1: price,
-    part2: "",
+    part1: getPrice(plots, false),
+    part2: getPrice(plots, true),
   };
 };
 
-type PlotInfo = Record<string, { area: number; perimeter: number }[]>;
+type PlotInfo = Record<string, { area: number; perimeter: number; sides: number }[]>;
 
 const getPlotInfo = (grid: string[]) => {
   const visited = new Set<string>();
 
   const plots: PlotInfo = {};
+  const cornerCounts: Record<string, number> = {};
 
   const flood = (ch: string, r: number, c: number, blockIndex: number) => {
     if (visited.has(`${r},${c}`)) return;
     visited.add(`${r},${c}`);
     plots[ch][blockIndex].area++;
     plots[ch][blockIndex].perimeter += 4;
+
+    // check corner sides
+    [
+      [
+        [r, c - 1],
+        [r - 1, c],
+        [r - 1, c - 1],
+      ], // tl corner
+      [
+        [r, c + 1],
+        [r - 1, c],
+        [r - 1, c + 1],
+      ], // tr corner
+      [
+        [r, c - 1],
+        [r + 1, c],
+        [r + 1, c - 1],
+      ], // bl corner
+      [
+        [r, c + 1],
+        [r + 1, c],
+        [r + 1, c + 1],
+      ], // br corner
+    ].forEach(([[r1, c1], [r2, c2], [r3, c3]]) => {
+      const side1 = grid[r1]?.[c1];
+      const side2 = grid[r2]?.[c2];
+      const diag = grid[r3]?.[c3];
+
+      if (diag === ch && side1 !== ch && side2 !== ch) {
+        if (!cornerCounts[`${ch},${blockIndex}`]) cornerCounts[`${ch},${blockIndex}`] = 0;
+        cornerCounts[`${ch},${blockIndex}`] += 1;
+      } else if (side1 !== ch && side2 !== ch) {
+        plots[ch][blockIndex].sides++;
+      }
+      // let count = 1;
+
+      // const ch1 = grid[r1]?.[c1];
+      // const ch2 = grid[r2]?.[c2];
+      // const ch3 = grid[r3]?.[c3];
+      // const count = [ch1, ch2, ch3].reduce((a, gCh) => (ch === gCh ? a + 1 : a), 0) + 1;
+
+      // if (count % 2 === 1) {
+      //   if (count === 3) {
+      //     if (!cornerCounts[`${ch},${blockIndex}`])
+      //       cornerCounts[`${ch},${blockIndex}`] = 0;
+      //     cornerCounts[`${ch},${blockIndex}`] += 1;
+      //   } else {
+      //     plots[ch][blockIndex].sides++;
+      //   }
+      // }
+    });
 
     DIRS.forEach(([dr, dc]) => {
       const nr = r + dr;
@@ -45,8 +95,11 @@ const getPlotInfo = (grid: string[]) => {
         plots[ch] = [];
       }
       if (!visited.has(`${i},${j}`)) {
-        plots[ch].push({ area: 0, perimeter: 0 });
-        flood(ch, i, j, plots[ch].length - 1);
+        plots[ch].push({ area: 0, perimeter: 0, sides: 0 });
+
+        const blockIndex = plots[ch].length - 1;
+        flood(ch, i, j, blockIndex);
+        plots[ch][blockIndex].sides += (cornerCounts[`${ch},${blockIndex}`] ?? 0) / 3;
       }
     }
   }
@@ -54,15 +107,16 @@ const getPlotInfo = (grid: string[]) => {
   return plots;
 };
 
-const getPrice = (plots: PlotInfo) => {
+const getPrice = (plots: PlotInfo, useSides: boolean) => {
   return Object.entries(plots).reduce((acc, [ch, blocks]) => {
     return (
       acc +
-      blocks.reduce((acc, { area, perimeter }) => {
-        return acc + area * perimeter;
+      blocks.reduce((acc, { area, perimeter, sides }) => {
+        return acc + (useSides ? sides : perimeter) * area;
       }, 0)
     );
   }, 0);
 };
 
 // 1378316 too high
+1375574;
