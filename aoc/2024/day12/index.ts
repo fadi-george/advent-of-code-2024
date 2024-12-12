ba982056bfd75062e4ed0ec55af8de7394954a99
Fadi George <fadii925@gmail.com>
2024-12-12 01:53:11 -0800

    slight clean up

--- aoc/2024/day12/index.ts
+++ aoc/2024/day12/index.ts
@@ -3,19 +3,28 @@ import { DIRS } from "../../constants";
 export default (input: string) => {
   const grid = input.split("\n");
   const plots = getPlotInfo(grid);
+
   return {
     part1: getPrice(plots, false),
     part2: getPrice(plots, true),
   };
 };
 
+// prettier-ignore
+const CORNER_OFFSETS = [[0, -1], [-1, 0], [-1, -1]]; // left, top, diagonal
+const ALL_CORNERS = [
+  CORNER_OFFSETS, // top-left direction
+  CORNER_OFFSETS.map(([r, c]) => [r, -c]), // top-right
+  CORNER_OFFSETS.map(([r, c]) => [-r, c]), // bottom-left
+  CORNER_OFFSETS.map(([r, c]) => [-r, -c]), // bottom-right
+];
 type PlotInfo = Record<string, { area: number; perimeter: number; sides: number }[]>;
 
 const getPlotInfo = (grid: string[]) => {
   const visited = new Set<string>();
 
   const plots: PlotInfo = {};
-  const cornerCounts: Record<string, number> = {};
+  const invCorners: Record<string, number> = {};
 
   const flood = (ch: string, r: number, c: number, blockIndex: number) => {
     if (visited.has(`${r},${c}`)) return;
@@ -23,50 +32,28 @@ const getPlotInfo = (grid: string[]) => {
     plots[ch][blockIndex].area++;
     plots[ch][blockIndex].perimeter += 4;
 
-    // check corner sides
-    [
-      [
-        [r, c - 1],
-        [r - 1, c],
-        [r - 1, c - 1],
-      ], // tl corner
-      [
-        [r, c + 1],
-        [r - 1, c],
-        [r - 1, c + 1],
-      ], // tr corner
-      [
-        [r, c - 1],
-        [r + 1, c],
-        [r + 1, c - 1],
-      ], // bl corner
-      [
-        [r, c + 1],
-        [r + 1, c],
-        [r + 1, c + 1],
-      ], // br corner
-    ].forEach(([[r1, c1], [r2, c2], [r3, c3]]) => {
-      const side1 = grid[r1]?.[c1];
-      const side2 = grid[r2]?.[c2];
-      const diag = grid[r3]?.[c3];
+    ALL_CORNERS.forEach(([[dr1, dc1], [dr2, dc2], [dr3, dc3]]) => {
+      const side1 = grid[r + dr1]?.[c + dc1];
+      const side2 = grid[r + dr2]?.[c + dc2];
+      const diag = grid[r + dr3]?.[c + dc3];
 
+      // no same neighbors in corner direction
       if (side1 !== ch && side2 !== ch) {
         plots[ch][blockIndex].sides++;
       } else if (
+        // two same neighbors in corner direction
         (ch === side1 && ch === side2 && diag !== ch) ||
         (ch === diag && (side1 === ch || side2 === ch) && side1 !== side2)
       ) {
-        if (!cornerCounts[`${ch},${blockIndex}`]) cornerCounts[`${ch},${blockIndex}`] = 0;
-        cornerCounts[`${ch},${blockIndex}`] += 1;
+        // inverse corners would be counted 3 times which we will adjust later
+        if (!invCorners[`${ch},${blockIndex}`]) invCorners[`${ch},${blockIndex}`] = 0;
+        invCorners[`${ch},${blockIndex}`] += 1;
       }
     });
 
     DIRS.forEach(([dr, dc]) => {
-      const nr = r + dr;
-      const nc = c + dc;
-      const nCh = grid[nr]?.[nc];
-      if (!nCh) return;
-      if (nCh === ch) {
+      const [nr, nc] = [r + dr, c + dc];
+      if (grid[nr]?.[nc] === ch) {
         plots[ch][blockIndex].perimeter--;
         flood(ch, nr, nc, blockIndex);
       }
@@ -76,15 +63,13 @@ const getPlotInfo = (grid: string[]) => {
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[i].length; j++) {
       const ch = grid[i][j];
-      if (!plots[ch]) {
-        plots[ch] = [];
-      }
+      if (!plots[ch]) plots[ch] = [];
       if (!visited.has(`${i},${j}`)) {
         plots[ch].push({ area: 0, perimeter: 0, sides: 0 });
 
         const blockIndex = plots[ch].length - 1;
         flood(ch, i, j, blockIndex);
-        plots[ch][blockIndex].sides += (cornerCounts[`${ch},${blockIndex}`] ?? 0) / 3;
+        plots[ch][blockIndex].sides += (invCorners[`${ch},${blockIndex}`] ?? 0) / 3;
       }
     }
   }
@@ -93,12 +78,6 @@ const getPlotInfo = (grid: string[]) => {
 };
 
 const getPrice = (plots: PlotInfo, useSides: boolean) =>
-  Object.entries(plots).reduce(
-    (acc, [, blocks]) =>
-      acc +
-      blocks.reduce(
-        (acc, { area, perimeter, sides }) => acc + (useSides ? sides : perimeter) * area,
-        0
-      ),
-    0
-  );
+  Object.values(plots)
+    .flat()
+    .reduce((sum, p) => sum + (useSides ? p.sides : p.perimeter) * p.area, 0);
