8b40592917f095bdb1203a672714f38628ae53f7
Fadi George <fadii925@gmail.com>
2024-12-17 01:25:35 -0800

    more clean up

--- aoc/2024/day17/index.ts
+++ aoc/2024/day17/index.ts
@@ -1,6 +1,8 @@
 import { compareArrays } from "../../lib/array";
 import { xor } from "../../lib/general";
 
+const [A, B, C] = [0, 1, 2];
+
 export default (input: string) => {
   const [registerLines, programLines] = input.split(/\n\n/);
   const registers = registerLines.split(/\n/).map((l) => +l.match(/\d+/)![0]);
@@ -12,63 +14,26 @@ export default (input: string) => {
   };
 };
 
-export const runProgram = (registers: number[], program: number[]) => {
-  let insPtr = 0;
-  let out: number[] = [];
+export const runProgram = (reg: number[], program: number[]) => {
+  let [ptr, out] = [0, [] as number[]];
 
-  const runLiteralOp = (op: number, comboOp: number) => {
-    let noJump = true;
+  const getVal = (op: number) => (op <= 3 ? op : reg[op - 4]);
+  const getAdv = (c: number) => Math.floor(reg[0] / Math.pow(2, getVal(c)));
 
-    const getAdv = () => Math.floor(registers[0] / Math.pow(2, getComboVal(comboOp)));
+  // NOTE: Not using ^ (xor) operator since it doesn't work well for large numbers
+  const ops: ((c: number) => number | boolean)[] = [
+    (c) => (reg[A] = getAdv(c)), // adv
+    (c) => (reg[B] = xor(reg[B], c)), // bxl
+    (c) => (reg[B] = getVal(c) % 8), // bst
+    (c) => reg[A] && ((ptr = c), true), // jnz
+    (c) => (reg[B] = xor(reg[B], reg[C])), // bxc
+    (c) => out.push(getVal(c) % 8), // out
+    (c) => (reg[B] = getAdv(c)), // bdv
+    (c) => (reg[C] = getAdv(c)), // cdv
+  ];
 
-    switch (op) {
-      case 0: // adv
-        registers[0] = getAdv();
-        break;
-      case 1: // bxl
-        registers[1] = xor(registers[1], comboOp);
-        break;
-      case 2: // bst
-        registers[1] = getComboVal(comboOp) % 8;
-        break;
-      case 3: // jnz
-        const AVal = registers[0];
-        if (AVal !== 0) {
-          insPtr = comboOp;
-          noJump = false;
-        }
-        break;
-      case 4: // bxc
-        registers[1] = xor(registers[1], registers[2]);
-        break;
-      case 5: // out
-        out.push(getComboVal(comboOp) % 8);
-        break;
-      case 6: // bdv
-        registers[1] = getAdv();
-        break;
-      case 7: // cdv
-        registers[2] = getAdv();
-        break;
-    }
-
-    return noJump;
-  };
-
-  const getComboVal = (op: number) => {
-    if (op <= 3) return op;
-    if (op === 4) return registers[0];
-    if (op === 5) return registers[1];
-    if (op === 6) return registers[2];
-    throw new Error(`Invalid combo operand: ${op}`);
-  };
-
-  while (insPtr < program.length) {
-    const noJump = runLiteralOp(program[insPtr], program[insPtr + 1]);
-    if (noJump) insPtr += 2;
-  }
-
-  return { out, registers };
+  while (ptr < program.length) if (ops[program[ptr]](program[ptr + 1]) !== true) ptr += 2;
+  return { out, registers: reg };
 };
 
 export const fixARegister = (program: number[]) => {
@@ -77,11 +42,10 @@ export const fixARegister = (program: number[]) => {
     const { out } = runProgram([a, 0, 0], program);
     if (out.length > program.length) return null;
 
-    if (compareArrays(out, program.slice(program.length - out.length))) {
-      if (out.length === program.length) break;
+    const target = program.slice(-out.length);
+    if (compareArrays(out, target)) {
+      if (out.length === program.length) return a;
       a *= 8;
     } else a++;
   }
-
-  return a;
 };
