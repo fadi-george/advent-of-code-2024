d12bcd533157ff8897e56475eca241da5b5d2798
Fadi George <fadii925@gmail.com>
2024-12-16 22:40:54 -0800

    day17p2 wip

--- aoc/2024/day17/day.test.ts
+++ aoc/2024/day17/day.test.ts
@@ -12,10 +12,10 @@ describe("day 17", () => {
     expect(registers[RegMap.B]).toEqual(1);
 
     ({ out } = runProgram([10, 0, 0], [5, 0, 5, 1, 5, 4]));
-    expect(out).toEqual("0,1,2");
+    expect(out.join(",")).toEqual("0,1,2");
 
     ({ out } = runProgram([2024, 0, 0], [0, 1, 5, 4, 3, 0]));
-    expect(out).toEqual("4,2,5,6,7,7,7,7,3,1,0");
+    expect(out.join(",")).toEqual("4,2,5,6,7,7,7,7,3,1,0");
 
     ({ out, registers } = runProgram([0, 29, 0], [1, 7]));
     expect(registers[RegMap.B]).toEqual(26);
@@ -30,9 +30,15 @@ describe("day 17", () => {
     expect(result.part1).toBe("4,6,3,5,6,3,5,2,1,0");
   });
 
-  // test("puzzle input", () => {
-  //   const input = readFile(`${dirname}/input.txt`);
-  //   const result = solution(input);
-  //   expect(result.part1).toBe("");
-  // });
+  test("example input 2", () => {
+    const input = readFile(`${dirname}/sample2.txt`);
+    const result = solution(input);
+    expect(result.part2).toBe(117440);
+  });
+
+  test("puzzle input", () => {
+    const input = readFile(`${dirname}/input.txt`);
+    const result = solution(input);
+    expect(result.part1).toBe("2,3,4,7,5,7,3,0,7");
+  });
 });
