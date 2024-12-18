db11b170bde2f374d09cda671a18f8268e828d18
Fadi George <fadii925@gmail.com>
2024-12-17 20:54:31 -0800

    day18 prep

---
+++ aoc/2024/day18/day.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "bun:test";
+import { readFile } from "../../lib/general";
+import solution from "./index";
+
+const dirname = import.meta.dirname;
+
+describe("day 18", () => {
+  test("example input", () => {
+    const input = readFile(`${dirname}/sample.txt`);
+    const result = solution(input);
+    expect(result.part1).toBe(0);
+  });
+
+  test("puzzle input", () => {
+    const input = readFile(`${dirname}/input.txt`);
+    const result = solution(input);
+    expect(result.part1).toBe(0);
+  });
+});
---
+++ aoc/2024/day18/index.ts
@@ -0,0 +1,8 @@
+export default (input: string) => {
+  input.split("");
+
+  return {
+    part1: 0,
+    part2: 0,
+  };
+};
