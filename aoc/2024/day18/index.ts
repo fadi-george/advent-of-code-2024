a1ce76ffdd4da437ef35a1d77f0be496da2129f9
Fadi George <fadii925@gmail.com>
2024-12-17 22:23:19 -0800

    clean up

--- aoc/2024/day18/index.ts
+++ aoc/2024/day18/index.ts
@@ -9,66 +9,45 @@ export default (input: string, size = SIZE, bytes = BYTES) => {
   const byteCoords = lines.map((l) => l.split(",").map(Number));
 
   const grid = makeGrid(size, size, ".");
-
   byteCoords.slice(0, bytes).forEach(([x, y]) => (grid[y][x] = "#"));
 
   return {
-    part1: solve(grid),
+    part1: getMinSteps(grid),
     part2: findBlockingByte(size, byteCoords),
   };
 };
 
-type Info = {
-  r: number;
-  c: number;
-  steps: number;
-};
+type Info = { r: number; c: number; steps: number };
 
-const solve = (grid: string[][]) => {
-  const visited = new Map<string, number>();
+const getMinSteps = (grid: string[][]) => {
+  const [size, visited] = [grid.length, new Set<string>()];
+
   const pq = new MinPriorityQueue((v: Info) => v.steps);
   pq.enqueue({ r: 0, c: 0, steps: 0 });
 
-  const size = grid.length;
-  let minSteps = Infinity;
-
   while (!pq.isEmpty()) {
     const { r, c, steps } = pq.dequeue();
-    if (r === size - 1 && c === size - 1) {
-      minSteps = steps;
-      break;
-    }
+    if (r === size - 1 && c === size - 1) return steps;
 
     const key = `${r},${c}`;
     if (visited.has(key)) continue;
-    visited.set(key, steps);
+    visited.add(key);
 
-    for (const [dr, dc] of DIRS) {
-      const nr = r + dr;
-      const nc = c + dc;
-      const ch = grid[nr]?.[nc];
-      if (!ch || ch === "#") continue;
-      pq.enqueue({ r: nr, c: nc, steps: steps + 1 });
-    }
+    for (const [dr, dc] of DIRS)
+      if (grid[r + dr]?.[c + dc] === ".")
+        pq.enqueue({ r: r + dr, c: c + dc, steps: steps + 1 });
   }
-
-  return minSteps;
+  return Infinity;
 };
 
 const findBlockingByte = (size: number, coords: number[][]) => {
-  const grid = Array.from({ length: size }, () =>
-    Array.from({ length: size }, () => ".")
-  );
+  const grid = makeGrid(size, size, ".");
+  coords.forEach(([x, y]) => (grid[y][x] = "#"));
 
-  coords.forEach(([x, y]) => {
-    grid[y][x] = "#";
-  });
-
+  // working backwards, remove each byte and check if it's possible to reach the end
   for (let i = coords.length - 1; i >= 0; i--) {
     const [x, y] = coords[i];
     grid[y][x] = ".";
-    let steps = solve(grid);
-    if (steps !== Infinity) return coords[i].join(",");
+    if (getMinSteps(grid) !== Infinity) return [x, y].join(",");
   }
-  return null;
 };
