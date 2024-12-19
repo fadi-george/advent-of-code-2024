6c7a57d91e1f412a914bf0f0c603586b67824dfd
Fadi George <fadii925@gmail.com>
2024-12-18 21:11:47 -0800

    day19 p1

--- aoc/2024/day19/day.test.ts
+++ aoc/2024/day19/day.test.ts
@@ -8,14 +8,16 @@ describe("day 19", () => {
   test("example input", () => {
     const input = readFile(`${dirname}/sample.txt`);
     const result = solution(input);
-    expect(result.part1).toBe("");
-    expect(result.part2).toBe("");
+    expect(result.part1).toBe(6);
+    // expect(result.part2).toBe("");
   });
 
-  test("puzzle input", () => {
-    const input = readFile(`${dirname}/input.txt`);
-    const result = solution(input);
-    expect(result.part1).toBe("");
-    expect(result.part2).toBe("");
-  });
+  // test("puzzle input", () => {
+  //   const input = readFile(`${dirname}/input.txt`);
+  //   const result = solution(input);
+  //   expect(result.part1).toBe("");
+  //   // expect(result.part2).toBe("");
+  // });
 });
+
+// 301 - wrong
--- aoc/2024/day19/index.ts
+++ aoc/2024/day19/index.ts
@@ -1,4 +1,47 @@
 export default function solution(input: string) {
-  input.split(/\n/);
-  return { part1: "", part2: "" };
+  const [towelPatterns, onsenLine] = input.split(/\n\n/);
+  const patterns = new Set(towelPatterns.split(", "));
+  const onsenPatterns = onsenLine.split(/\n/);
+  // console.log({ onsenPatterns, patterns });
+
+  const p1 = validatePatterns(patterns, onsenPatterns);
+  return { part1: p1, part2: "" };
 }
+
+const validatePatterns = (patternSet: Set<string>, onsenPatterns: string[]) => {
+  // console.log({ onsenPatterns, patternSet });
+  const patterns = [...patternSet].sort((a, b) => b.length - a.length);
+  const patternsRev = [...patterns].reverse();
+  // console.log({ patterns });
+  let validPatterns = [];
+
+  for (const onsenPattern of onsenPatterns) {
+    let str = onsenPattern;
+    for (const pattern of patterns) {
+      if (onsenPattern.includes(pattern)) {
+        str = str.replaceAll(pattern, "");
+      }
+    }
+    if (str.length === 0) {
+      validPatterns.push(onsenPattern);
+      continue;
+    }
+
+    str = onsenPattern;
+    for (const pattern of patternsRev) {
+      if (onsenPattern.includes(pattern)) {
+        str = str.replaceAll(pattern, "");
+      }
+    }
+    if (str.length === 0) {
+      validPatterns.push(onsenPattern);
+    }
+    // console.log({ str });
+  }
+  // console.log({ validPatterns });
+
+  return validPatterns.length;
+};
+
+// 301 wrong
+// 342 ???
