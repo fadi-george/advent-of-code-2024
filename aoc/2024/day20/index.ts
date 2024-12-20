a2b12bff203bfb84058aef549ec7ba7f1573fa6e
Fadi George <fadii925@gmail.com>
2024-12-20 15:06:35 -0800

    optimize by doing one run of getting cheats

--- aoc/2024/day20/index.ts
+++ aoc/2024/day20/index.ts
@@ -8,20 +8,18 @@ export default function solution(input: string) {
   const [start, end] = [findInGrid(grid, "S"), findInGrid(grid, "E")];
   const bestPath = getBestPath(grid, start, end);
 
-  return {
-    part1: getCheatScore(bestPath, 2),
-    part2: getCheatScore(bestPath, 20),
-  };
+  return getCheatScore(bestPath);
 }
 
-const [ValidChars, SAVE_THRESHOLD] = [[".", "E"], 100];
+const [ValidChars, SAVE_THRESHOLD, CHEAT_LIMIT] = [[".", "E"], 100, 20];
 
 export const getBestPath = (grid: Grid, start: Point, end: Point) => {
   const visited = new Set<string>();
-  const q = [{ r: start[0], c: start[1], path: [start] }];
+  const q = [[start]];
 
   while (q.length > 0) {
-    const { r, c, path } = q.shift()!;
+    const path = q.shift()!;
+    const [r, c] = path[path.length - 1];
     if (r === end[0] && c === end[1]) return path;
 
     if (visited.has(`${r},${c}`)) continue;
@@ -29,22 +27,25 @@ export const getBestPath = (grid: Grid, start: Point, end: Point) => {
 
     for (const [dr, dc] of DIRS)
       if (ValidChars.includes(grid[r + dr]?.[c + dc]))
-        q.push({ r: r + dr, c: c + dc, path: [...path, [r + dr, c + dc]] });
+        q.push([...path, [r + dr, c + dc]]);
   }
   return [];
 };
 
-export const getCheatScore = (bestPath: Point[], cheatLimit: number) => {
-  let result = 0;
+export const getCheatScore = (bestPath: Point[]) => {
+  let [p1, p2] = [0, 0];
 
-  // All values in path are free space so cutting through walls will land you back on a free space
-  // if we use manhatten distance. Then it is a mater of checking if we're within the cheat limit.
+  // All values in best path are free space so cutting through walls will land you back on a free space
+  // if we use Manhattan distance. Then it is a mater of checking if we're within the cheat limit.
   for (let i = 0; i < bestPath.length; i++)
     for (let j = i + 1; j < bestPath.length; j++) {
       const distance = manhattanDistance(bestPath[i], bestPath[j]);
       const stepDiff = j - i;
-      if (distance <= cheatLimit && stepDiff - distance >= SAVE_THRESHOLD) result += 1;
+      if (distance <= CHEAT_LIMIT && stepDiff - distance >= SAVE_THRESHOLD) {
+        p1 += distance <= 2 ? 1 : 0;
+        p2 += 1;
+      }
     }
 
-  return result;
+  return { part1: p1, part2: p2 };
 };
