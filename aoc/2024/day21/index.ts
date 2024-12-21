17596b158025ab80616c988496035a3ed32da381
Fadi George <fadii925@gmail.com>
2024-12-21 13:37:02 -0800

    more clean up

--- aoc/2024/day21/index.ts
+++ aoc/2024/day21/index.ts
@@ -1,6 +1,6 @@
-import { MinPriorityQueue } from "@datastructures-js/priority-queue";
 import { memoize } from "../../lib/general";
 import { sum } from "../../lib/array";
+import { NUM_KEY_MAP, DIR_KEY_MAP } from "./constants";
 
 export default function solution(input: string) {
   const keys = input.split(/\n/);
@@ -10,58 +10,21 @@ export default function solution(input: string) {
 const solve = (keys: string[], robots: number) => {
   let res = 0;
   keys.forEach((key) => {
-    const num = parseInt(key.match(/\d+/)?.[0] ?? "0");
+    const num = parseInt(key.match(/\d+/)![0]);
     const minLength = checkChunk(key, robots);
     res += num * minLength;
   });
   return res;
 };
 
-// Quick Lookup for Numeric Pad
-// ["7", "8", "9"],
-// ["4", "5", "6"],
-// ["1", "2", "3"],
-// [" ", "0", "A"],
-const NUM_KEY_MAP = {
-  "7": [0, 0],
-  "8": [0, 1],
-  "9": [0, 2],
-  "4": [1, 0],
-  "5": [1, 1],
-  "6": [1, 2],
-  "1": [2, 0],
-  "2": [2, 1],
-  "3": [2, 2],
-  " ": [3, 0],
-  "0": [3, 1],
-  A: [3, 2],
-};
-
-// Quick Lookup for Direction Keys for
-// [" ", "^", "A"],
-// ["<", "v", ">"],
-const DIR_KEY_MAP = {
-  " ": [0, 0],
-  "^": [0, 1],
-  A: [0, 2],
-  "<": [1, 0],
-  v: [1, 1],
-  ">": [1, 2],
-};
-
-type Info = { r: number; c: number; str: string; i: number };
-
 const getDirs = (sequence: string, isNumeric: boolean) => {
-  const q = new MinPriorityQueue((v: Info) => v.str.length);
   const keyMap = isNumeric ? NUM_KEY_MAP : DIR_KEY_MAP;
-  const start = keyMap["A"]; // start at 'A' button
-  const blank = keyMap[" "]; // blank key position
-
-  q.enqueue({ r: start[0], c: start[1], str: "", i: 0 });
+  const [start, blank] = [keyMap["A"], keyMap[" "]];
+  const q = [{ r: start[0], c: start[1], str: "", i: 0 }];
   const res: string[] = [];
 
-  while (!q.isEmpty()) {
-    const { r, c, str, i } = q.dequeue()!;
+  while (q.length > 0) {
+    const { r, c, str, i } = q.shift()!;
 
     if (i >= sequence.length) {
       res.push(str);
@@ -77,15 +40,15 @@ const getDirs = (sequence: string, isNumeric: boolean) => {
 
     // arrived at next key in sequence so can just press 'A
     if (rDiff === 0 && cDiff === 0) {
-      q.enqueue({ r, c, str: str + "A", i: i + 1 });
+      q.push({ r, c, str: str + "A", i: i + 1 });
       continue;
     }
 
     // move up, down, left, right
-    if (rDiff < 0) q.enqueue({ r: r - 1, c: c, str: str + "^", i });
-    else if (rDiff > 0) q.enqueue({ r: r + 1, c: c, str: str + "v", i });
-    if (cDiff < 0) q.enqueue({ r: r, c: c - 1, str: str + "<", i });
-    else if (cDiff > 0) q.enqueue({ r: r, c: c + 1, str: str + ">", i });
+    if (rDiff < 0) q.push({ r: r - 1, c: c, str: str + "^", i });
+    else if (rDiff > 0) q.push({ r: r + 1, c: c, str: str + "v", i });
+    if (cDiff < 0) q.push({ r: r, c: c - 1, str: str + "<", i });
+    else if (cDiff > 0) q.push({ r: r, c: c + 1, str: str + ">", i });
   }
   return res;
 };
@@ -96,8 +59,8 @@ const checkChunk = memoize((chunk: string, depth: number): number => {
 
   let minLength = Infinity;
   for (const way of ways) {
-    // split into chunks that end with 'A', some sub-chunks might might be the same for all different ways
-    // other sub-chunks might be lead to shorter strings e.g. '^^A>vA<A' -> ['^^A', '>vA', '<A']
+    // split into chunks that end with 'A' e.g. '^^A>vA<A' -> ['^^A', '>vA', '<A'], some sub-chunks might might be the same for all different ways
+    // other sub-chunks might be lead to shorter strings
     const chunks = way.match(/[^A]*A/g)!;
     minLength = Math.min(minLength, sum(chunks.map((c) => checkChunk(c, depth - 1))));
   }
