ab41b5770b54fa3e2429d548a219e0172bf544c0
Fadi George <fadii925@gmail.com>
2024-12-23 01:14:34 -0800

    update tests

--- aoc/2024/day23/day.test.ts
+++ aoc/2024/day23/day.test.ts
@@ -8,13 +8,14 @@ describe("day 23", () => {
   test("example input", () => {
     const input = readFile(`${dirname}/sample.txt`);
     const result = solution(input);
-    expect(result.part1).toBe("");
+    expect(result.part1).toBe(7);
+    expect(result.part2).toBe("co,de,ka,ta");
   });
 
   test("puzzle input", () => {
     const input = readFile(`${dirname}/input.txt`);
     const result = solution(input);
-    expect(result.part1).toBe("");
-    expect(result.part2).toBe("");
+    expect(result.part1).toBe(1046);
+    expect(result.part2).toBe("de,id,ke,ls,po,sn,tf,tl,tm,uj,un,xw,yz");
   });
 });
