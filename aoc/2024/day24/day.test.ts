e2542142642d1cd94b1a538b8fc0ce69e63136d7
Fadi George <fadii925@gmail.com>
2024-12-23 20:59:36 -0800

    prep day 24

---
+++ aoc/2024/day24/day.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from "bun:test";
+import { readFile } from "../../lib/general";
+import solution from "./index";
+
+const dirname = import.meta.dirname;
+
+describe("day 24", () => {
+  test("example input", () => {
+    const input = readFile(`${dirname}/sample.txt`);
+    const result = solution(input);
+    // expect(result.part1).toBe(7);
+    // expect(result.part2).toBe("co,de,ka,ta");
+  });
+
+  test("puzzle input", () => {
+    const input = readFile(`${dirname}/input.txt`);
+    const result = solution(input);
+    // expect(result.part1).toBe(1046);
+    // expect(result.part2).toBe("de,id,ke,ls,po,sn,tf,tl,tm,uj,un,xw,yz");
+  });
+});
---
+++ aoc/2024/day24/index.ts
@@ -0,0 +1,3 @@
+export default function solution(input: string) {
+  return { part1: 0, part2: 0 };
+}
