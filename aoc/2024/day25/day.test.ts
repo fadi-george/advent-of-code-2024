3b5f0722651d17fa9e1b2539466a0bdb47eeaddf
Fadi George <fadii925@gmail.com>
2024-12-24 20:56:19 -0800

    prep day25

---
+++ aoc/2024/day25/day.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from "bun:test";
+import { readFile } from "../../lib/general";
+import solution from "./index";
+
+const dirname = import.meta.dirname;
+
+describe("day 5", () => {
+  test("example input", () => {
+    const input = readFile(`${dirname}/sample.txt`);
+    const result = solution(input);
+    expect(result.part1).toBe("");
+  });
+
+  test("puzzle input", () => {
+    const input = readFile(`${dirname}/input.txt`);
+    const result = solution(input);
+    expect(result.part1).toBe("");
+  });
+});
